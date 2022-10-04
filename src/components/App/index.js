f21dc2cad7386a1ac612d0a5ba9245b44177fa0e
Eduardo Pinto <edfepinto@gmail.com>
2022-10-04 10:33:26 -0300

    created a contacts header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ import defaultTheme from '../../assets/styles/themes/default';
 import { Container } from './styles';
 
 import Header from '../Header';
+import ContactsList from '../ContactsList';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <GlobalStyles />
       <Container>
         <Header />
+        <ContactsList />
       </Container>
     </ThemeProvider>
   );
---
+++ src/components/ContactsList/index.js
@@ -0,0 +1,12 @@
+import { Container, Header } from './styles';
+
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 contacts</strong>
+        <a href="/">New Contacts</a>
+      </Header>
+    </Container>
+  );
+}
