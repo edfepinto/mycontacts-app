271379b6620743c522cdf15a6c8e46309cc26937
Eduardo Pinto <edfepinto@gmail.com>
2022-10-05 10:26:00 -0300

    created a button, input and select components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,24 @@
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function NewContact() {
-  return <PageHeader title="New Contact" />;
+  return (
+    <>
+      <PageHeader title="New Contact" />
+      <Input type="text" placeholder="Name" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button type="button">
+        Save changes
+      </Button>
+      <Button type="button" disabled>
+        Save changes
+      </Button>
+    </>
+  );
 }
