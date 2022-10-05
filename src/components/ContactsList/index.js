58007bd7c8492bbc1e5ff77a130bd8f61f72857a
Eduardo Pinto <edfepinto@gmail.com>
2022-10-05 09:12:30 -0300

    added a contacts listage

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/svg/icons/arrow.svg';
+import edit from '../../assets/images/svg/icons/edit.svg';
+import trash from '../../assets/images/svg/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +13,75 @@ export default function ContactsList() {
         <strong>3 contacts</strong>
         <a href="/">New Contacts</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Name</span>
+            <img src={arrow} alt="ordernation button" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Pinto</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo@email.com</span>
+            <span>(54) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit contact" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete contact" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Pinto</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo@email.com</span>
+            <span>(54) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit contact" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete contact" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Pinto</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo@email.com</span>
+            <span>(54) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit contact" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete contact" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
