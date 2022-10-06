772efb4a585806e73b4168d36a8196f820acf0c2
Eduardo Pinto <edfepinto@gmail.com>
2022-10-06 10:24:47 -0300

    created a name and email validation with controlled component

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+
+import isEmailValid from '../../utils/isEmailValid';
 
 import FormGroup from '../FormGroup';
 import Input from '../Input';
@@ -8,23 +11,86 @@ import Button from '../Button';
 import { ButtonContainer, Form } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Name is required' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'name',
+      ));
+    }
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid()) {
+      const errorAlreadyExists = errors.find((error) => (
+        error.field === 'email'));
+
+      if (errorAlreadyExists) return;
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'Email is invalid' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'email',
+      ));
+    }
+  };
+
+  const handleSubmit = (event) => event.preventDefault();
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input type="text" placeholder="Name" />
-      </FormGroup>
-
-      <FormGroup error="Email format is invalid">
-        <Input type="text" placeholder="E-mail" error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input type="text" placeholder="Telefone" />
+        <Input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          type="text"
+          placeholder="Phone"
+          value={phone}
+          onChange={({ target }) => setPhone(target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={({ target }) => setCategory(target.value)}
+        >
+          <option value="">Category</option>
           <option value="Instagram">Instagram</option>
+          <option value="Discord">Discord</option>
         </Select>
       </FormGroup>
 
