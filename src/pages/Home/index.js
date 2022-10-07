d01379f053e911695e781d1973bf22726f71d747
Eduardo Pinto <edfepinto@gmail.com>
2022-10-07 12:14:24 -0300

    created a contact ordenation function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
-  Container, Header, ListContainer, Card, InputSearchContainer,
+  Container, Header, ListHeader, Card, InputSearchContainer,
 } from './styles';
 
 import arrow from '../../assets/images/svg/icons/arrow.svg';
@@ -11,9 +11,10 @@ import trash from '../../assets/images/svg/icons/trash.svg';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
 
   useEffect(() => {
-    fetch('http://localhost:3001/contacts')
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
         const json = await response.json();
         setContacts(json);
@@ -21,7 +22,13 @@ export default function Home() {
       .catch((error) => {
         console.log('Internal Server Error', error);
       });
-  }, []);
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy(
+      (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
+    );
+  }
 
   return (
     <Container>
@@ -40,38 +47,36 @@ export default function Home() {
         <Link to="/new">New Contact</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span>Name</span>
-            <img src={arrow} alt="ordernation button" />
-          </button>
-        </header>
+      <ListHeader orderBy={orderBy}>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span>Name</span>
+          <img src={arrow} alt="ordernation button" />
+        </button>
+      </ListHeader>
 
-        {contacts.map((contact) => (
-          <Card key={contact.id}>
-            <div className="info">
-              <div className="contact-name">
-                <strong>{contact.name}</strong>
-                {contact.category_name && (
-                  <small>{contact.category_name}</small>
-                )}
-              </div>
-              <span>{contact.email}</span>
-              <span>{contact.phone}</span>
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              {contact.category_name && (
+                <small>{contact.category_name}</small>
+              )}
             </div>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
 
-            <div className="actions">
-              <Link to={`/edit/${contact.id}`}>
-                <img src={edit} alt="edit contact" />
-              </Link>
-              <button type="button">
-                <img src={trash} alt="delete contact" />
-              </button>
-            </div>
-          </Card>
-        ))}
-      </ListContainer>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={edit} alt="edit contact" />
+            </Link>
+            <button type="button">
+              <img src={trash} alt="delete contact" />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -49,25 +49,29 @@ export const InputSearchContainer = styled.div`
   }
 `;
 
-export const ListContainer = styled.div`
+export const ListHeader = styled.div`
   margin-top: 24px;
+  margin-bottom: 8px;
 
-  header {
-    margin-bottom: 8px;
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
 
-    button {
-      background: transparent;
-      border: none;
-      display: flex;
-      align-items: center;
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
 
-      span {
-        margin-right: 8px;
-        font-weight: bold;
-        color: ${({ theme }) => theme.colors.primary.main};
+    img {
+      transform: ${({ orderBy }) => (orderBy === 'asc'
+    ? 'rotate(180deg)'
+    : 'rotate(0deg)')};
+      transition: transform 0.2s ease-in;
       }
     }
-  }
 `;
 
 export const Card = styled.div`
