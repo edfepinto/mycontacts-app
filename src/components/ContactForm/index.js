4af7dd678dcb31b759eff2f727a3952414b4a93e
Eduardo Pinto <edfepinto@gmail.com>
2022-10-10 16:46:05 -0300

    implemented a create contacts function

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -14,7 +14,7 @@ import CategoryService from '../../services/CategoryService';
 
 import { ButtonContainer, Form } from './styles';
 
-export default function ContactForm({ buttonLabel }) {
+export default function ContactForm({ buttonLabel, onSubmit }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -71,6 +71,10 @@ export default function ContactForm({ buttonLabel }) {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    onSubmit({
+      name, email, phone, categoryId,
+    });
   }
 
   return (
@@ -135,4 +139,5 @@ export default function ContactForm({ buttonLabel }) {
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,11 +1,32 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContacts(contact);
+
+      console.log(response);
+    } catch {
+      alert('Create user Error');
+    }
+  }
+
   return (
     <>
       <PageHeader title="New Contact" />
-      <ContactForm buttonLabel="Register" />
+      <ContactForm
+        buttonLabel="Register"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
