261a944ada72368b9a7bc66fa1a7e12bcc965424
Eduardo Pinto <edfepinto@gmail.com>
2022-10-10 17:11:16 -0300

    refactored httpclient

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -10,7 +10,7 @@ class ContactsService {
   }
 
   async createContacts(contact) {
-    return this.httpClient.post('/contacts', contact);
+    return this.httpClient.post('/contacts', { body: contact });
   }
 }
 
