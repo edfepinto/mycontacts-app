271b7db1a2c0205d06f00da6f5f7599eb538209f
Eduardo Pinto <edfepinto@gmail.com>
2022-10-13 08:43:10 -0300

    improvement react portals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,9 @@
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import Button from '../Button';
 
 import { Overlay, Container, Footer } from './styles';
+import ReactPortal from '../ReactPortal';
 
 export default function Modal({
   danger,
@@ -18,37 +18,38 @@ export default function Modal({
 }) {
   if (!visible) return null;
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
 
-        <div className="modal-body">
-          {children}
-        </div>
+          <div className="modal-body">
+            {children}
+          </div>
 
-        <Footer>
-          <button
-            type="button"
-            className="cancel-button"
-            onClick={onCancel}
-            disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
 
-          <Button
-            type="button"
-            danger={danger}
-            onClick={onConfirm}
-            isLoading={isLoading}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root'),
+            <Button
+              type="button"
+              danger={danger}
+              onClick={onConfirm}
+              isLoading={isLoading}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
