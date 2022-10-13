ed9c12a6103abe2abab96e672426be9f3a078f86
Eduardo Pinto <edfepinto@gmail.com>
2022-10-13 09:15:36 -0300

    fixed react warning to edit contact not found

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,11 +1,16 @@
 import {
-  useState, useEffect, forwardRef, useImperativeHandle,
+  useState,
+  useEffect,
+  forwardRef,
+  useImperativeHandle,
 } from 'react';
 import PropTypes from 'prop-types';
 
 import isEmailValid from '../../utils/isEmailValid';
 import formatPhone from '../../utils/formatPhone';
+
 import useErrors from '../../hooks/useErrors';
+import useSafeAsyncState from '../../hooks/useSafeAsyncState';
 
 import FormGroup from '../FormGroup';
 import Input from '../Input';
@@ -21,8 +26,8 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [categoryId, setCategoryId] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [categories, setCategories] = useSafeAsyncState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const {
@@ -61,7 +66,7 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     }
 
     loadCategories();
-  }, []);
+  }, [setCategories, setIsLoadingCategories]);
 
   function handleNameChange(event) {
     setName(event.target.value);
---
+++ src/hooks/useSafeAsyncState.js
@@ -0,0 +1,26 @@
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+export default function useSafeAsyncState(initialState) {
+  const [state, setState] = useState(initialState);
+
+  const isMounted = useRef();
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const setSafeAsyncState = useCallback((data) => {
+    if (isMounted.current) setState(data);
+  }, []);
+
+  return [state, setSafeAsyncState];
+}
