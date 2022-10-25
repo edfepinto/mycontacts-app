8cc58945ad157cee47bf35ec9272f48886ba294c
Eduardo Pinto <edfepinto@gmail.com>
2022-10-24 21:47:39 -0300

    started construction of Contact Mapper

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,4 +1,5 @@
 import HttpClient from './utils/HttpClient';
+import ContactMapper from './mappers/ContactMapper';
 
 class ContactsService {
   constructor() {
@@ -14,11 +15,13 @@ class ContactsService {
   }
 
   createContacts(contact) {
-    return this.httpClient.post('/contacts', { body: contact });
+    const body = ContactMapper.toPersistence(contact);
+    return this.httpClient.post('/contacts', { body });
   }
 
   updateContact(id, contact) {
-    return this.httpClient.put(`/contacts/${id}`, { body: contact });
+    const body = ContactMapper.toPersistence(contact);
+    return this.httpClient.put(`/contacts/${id}`, { body });
   }
 
   deleteContact(id) {
